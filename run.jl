ab243a216244331ed5e0afd98d9ff640f8756de6
leonardgoeke <lgo@wip.tu-berlin.de>
2021-10-13 02:37:47 +0200

    updated repository

--- run.jl
+++ run.jl
@@ -1,21 +1,18 @@
-using AnyMOD, Gurobi
+using AnyMOD, Gurobi, YAML, CSV
+include("changeFormat.jl")
+
+emfFormat_df = DataFrame(timestep = String[], region = String[], variable = String[], scenario = String[], value = Float64[])
 
 # ! this branch requires specific AnyMOD version to support energy shares defined in 'par_setShare', version is installed using 'add https://github.com/leonardgoeke/AnyMOD.jl#dev_share'
 
 # initialize a model object, first two arguments are the input and output directory
 # (objName specifies a model name, shortExp specifies the distance of years (e.g. 2015, 2020 ...), decomm = :none deactivates endogenous decommissioning) 
-modelObj = anyModel("modelData","results", objName = "naEMF", shortExp = 5, decomm = :none)
+modelObj = anyModel("modelData","results", objName = "reference", shortExp = 5, supTsLvl = 2, decomm = :none)
 
 # create all variables and equations of the model
 createOptModel!(modelObj)
 # set the objective function
 setObjective!(:costs,modelObj)
-
-plotTree(:timestep,modelObj, plotSize = (64.0,8.0))
-plotTree(:region,modelObj, plotSize = (64.0,8.0))
-plotTree(:technology,modelObj, plotSize = (56.0,8.0))
-plotTree(:carrier,modelObj, plotSize = (20.0,8.0))
-
 # solve model with gurobi 
 set_optimizer(modelObj.optModel,Gurobi.Optimizer)
 set_optimizer_attribute(modelObj.optModel, "Method", 2); # set method option of gurobi to use barrier algorithm
@@ -23,7 +20,36 @@ set_optimizer_attribute(modelObj.optModel, "Crossover", 0); # disable crossover 
 optimize!(modelObj.optModel)
 
 # report results of solved model
-plotEnergyFlow(:sankey,modelObj); # writes an html file containing several sankey plots
+#plotEnergyFlow(:sankey,modelObj); # writes an html file containing several sankey plots
 reportResults(:summary,modelObj); # writes a pivot table summarizing key results
-reportResults(:exchange,modelObj); # writes a pivot table with more detailed results on interconnection
-reportResults(:costs,modelObj); # writes a pivot table with more detailed results on costs
+#reportResults(:exchange,modelObj); # writes a pivot table with more detailed results on interconnection
+#reportResults(:costs,modelObj); # writes a pivot table with more detailed results on costs
+
+append!(emfFormat_df,reportEMF(modelObj))
+
+#plotEnergyFlow(:sankey,modelObj, dropDown = (:timestep,))
+
+x = "net0by2080"
+for x in ["net0by2050","net0by2060","net0by2080"]
+    modelObj = anyModel(["modelData",x],"results", objName = x, shortExp = 5, supTsLvl = 2, decomm = :none)
+
+    # create all variables and equations of the model
+    createOptModel!(modelObj)
+    # set the objective function
+    setObjective!(:costs,modelObj)
+    # solve model with gurobi 
+    set_optimizer(modelObj.optModel,Gurobi.Optimizer)
+    set_optimizer_attribute(modelObj.optModel, "Method", 2); # set method option of gurobi to use barrier algorithm
+    set_optimizer_attribute(modelObj.optModel, "Crossover", 0); # disable crossover part of barrier algorithm
+    optimize!(modelObj.optModel)
+
+    # report results of solved model
+    #plotEnergyFlow(:sankey,modelObj); # writes an html file containing several sankey plots
+    reportResults(:summary,modelObj); #
+    reportResults(:exchange,modelObj); #
+#reportResults(:costs,modelObj); #
+
+    append!(emfFormat_df,reportEMF(modelObj))
+end
+
+CSV.write("results/emfFormat.csv", emfFormat_df)

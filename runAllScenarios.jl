cecdf7ca6da307448fa6ea65a5d2984d84ea7615
leonardgoeke <lgo@wip.tu-berlin.de>
2022-03-08 11:48:31 +0100

    added dac, made cmsg files and updated reporting

--- runAllScenarios.jl
+++ runAllScenarios.jl
@@ -2,11 +2,13 @@
 using AnyMOD, Gurobi, YAML, CSV
 include("changeFormat.jl")
 
+round = "R1"
+
 emfFormat_df = DataFrame(timestep = String[], region = String[], variable = String[], scenario = String[], value = Float64[])
 
 # initialize a model object, first two arguments are the input and output directory
 # (objName specifies a model name, shortExp specifies the distance of years (e.g. 2015, 2020 ...), decomm = :none deactivates endogenous decommissioning) 
-modelObj = anyModel("modelData","results", objName = "NT.Ref", shortExp = 5, supTsLvl = 2, decomm = :none)
+modelObj = anyModel(["modelData","Ref"],"results", objName = "NT.Ref." * round, shortExp = 5, supTsLvl = 2, decomm = :none)
 
 # create all variables and equations of the model
 createOptModel!(modelObj)
@@ -25,28 +27,45 @@ append!(emfFormat_df,reportEMF(modelObj))
 
 #plotEnergyFlow(:sankey,modelObj, dropDown = (:timestep,))
 
-scrName_dic = Dict("net0by2050" => "0by50.Ref", "net0by2060" => "0by60.Ref", "net0by2080" => "0by80.Ref")
+techName_dic = Dict("Ref" => "Ref", "NoDacCCS" => "CMSG.1", "AdvCCS" => "CMSG.2","AdvH2" => "CMSG.3","AdvDac" => "CMSG.4","AdvAll" => "Adv")
+scrName_dic = Dict("net0by2050" => "0by50", "net0by2060" => "0by60", "net0by2080" => "0by80")
 
-for x in ["net0by2050","net0by2060","net0by2080"]
-    modelObj = anyModel(["modelData",x],"results", objName = scrName_dic[x], shortExp = 5, supTsLvl = 2, decomm = :none)
+techName_dic = Dict("Ref" => "Ref", "AdvCCS" => "CMSG.2","AdvAll" => "Adv")
 
-    # create all variables and equations of the model
-    createOptModel!(modelObj)
-    
-    # set the objective function
-    setObjective!(:costs,modelObj)
 
-    # solve model with gurobi 
-    set_optimizer(modelObj.optModel,Gurobi.Optimizer)
-    set_optimizer_attribute(modelObj.optModel, "Method", 2); # set method option of gurobi to use barrier algorithm
-    set_optimizer_attribute(modelObj.optModel, "Crossover", 0); # disable crossover part of barrier algorithm
-    optimize!(modelObj.optModel)
+for y in keys(techName_dic)
+    println(y)
+    for x in keys(scrName_dic)
+        println(x)
+        modelObj = anyModel(["modelData",x,y],"results", objName = scrName_dic[x]  * "." *  techName_dic[y] * "." * round, shortExp = 5, supTsLvl = 2, decomm = :none)
 
-    # report results of solved model
-    reportResults(:summary,modelObj); #
-    reportResults(:exchange,modelObj); #
+        # create all variables and equations of the model
+        createOptModel!(modelObj)
+        
+        # set the objective function
+        setObjective!(:costs,modelObj)
 
-    append!(emfFormat_df,reportEMF(modelObj))
+        # solve model with gurobi 
+        set_optimizer(modelObj.optModel,Gurobi.Optimizer)
+        set_optimizer_attribute(modelObj.optModel, "Method", 2); # set method option of gurobi to use barrier algorithm
+        set_optimizer_attribute(modelObj.optModel, "Crossover", 0); # disable crossover part of barrier algorithm
+        optimize!(modelObj.optModel)
+
+        # report results of solved model
+        reportResults(:summary,modelObj); #
+        reportResults(:exchange,modelObj); #
+
+        append!(emfFormat_df,reportEMF(modelObj))
+    end
 end
-
 CSV.write("results/emfFormat.csv", emfFormat_df)
+
+
+bla = copy(emfFormat_df)
+
+bla[!,:value] = map(x -> x < 1e-5 ? 0.0 : x, emfFormat_df[!,:value])
+
+
+bla = unstack(bla, :timestep, :value)
+
+CSV.write("results/emfFormat2.csv", bla)
